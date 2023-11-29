d6d61d315915c977519180172d1a905be24ce9a0
Alex-Grigorovich <holdencolfield37@gmail.com>
2023-11-29 23:18:06 +0300

    Add: combo animation, state Player, light day and night.
    Add: combo animation, state Player, light day and night.

---
+++ Scripts/level.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@onready var light = $DirectionalLight2D
+
+enum {
+	MORNING,
+	DAY,
+	EVENING,
+	NIGHT
+}
+
+var state = MORNING
+
+func _ready():
+	light.enabled = true
+
+func _process(delta):
+	match state:
+		MORNING:
+			morning_state()
+		EVENING:
+			evening_state()
+
+func morning_state():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "energy", 0.2, 20)
+	
+func evening_state():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "energy", 0.95, 20)
+
+func _on_day_night_timeout():
+	if state < 3:
+		state += 1
+	else:
+		state = MORNING
