a1b1437ae822c09a9505e51e94d7f7b98087f21b
Alex-Grigorovich <holdencolfield37@gmail.com>
2023-12-04 23:44:58 +0300

    Add day count.
    Add day count.

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var light = $DirectionalLight2D
 @onready var point_light = $PointLight2D
 @onready var point_light2 = $PointLight2D2
+@onready var day_text = $CanvasLayer/DayText
+@onready var anim_player = $CanvasLayer/AnimationPlayer
 
 enum {
 	MORNING,
@@ -12,16 +14,13 @@ enum {
 }
 
 var state = MORNING
+var day_count: int
 
 func _ready():
 	light.enabled = true
-
-func _process(delta):
-	match state:
-		MORNING:
-			morning_state()
-		EVENING:
-			evening_state()
+	day_count = 1
+	set_day_text()
+	date_text_fade()
 
 func morning_state():
 	var tween = get_tree().create_tween()
@@ -46,7 +45,26 @@ func evening_state():
 	tween2.tween_property(point_light2, "energy", 1.5, 20)
 
 func _on_day_night_timeout():
+	match state:
+		MORNING:
+			morning_state()
+		EVENING:
+			evening_state()
+	
 	if state < 3:
 		state += 1
 	else:
 		state = MORNING
+		day_count += 1
+		set_day_text()
+		date_text_fade()
+
+func date_text_fade():
+	anim_player.play("day_text_fade_in")
+	await get_tree().create_timer(3).timeout
+	anim_player.play("day_text_fade_out")
+	
+	
+		
+func set_day_text():
+	day_text.text = "DAY " + str(day_count)
