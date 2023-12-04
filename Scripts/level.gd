2cecf0cc388724417e04ed6ad2826e6d5a4c2871
Alex-Grigorovich <holdencolfield37@gmail.com>
2023-12-04 23:09:45 +0300

    Add Point Light and shadows.
    Add Point Light and shadows.

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var light = $DirectionalLight2D
+@onready var point_light = $PointLight2D
+@onready var point_light2 = $PointLight2D2
 
 enum {
 	MORNING,
@@ -25,9 +27,23 @@ func morning_state():
 	var tween = get_tree().create_tween()
 	tween.tween_property(light, "energy", 0.2, 20)
 	
+	var tween1 = get_tree().create_tween()
+	tween1.tween_property(point_light, "energy", 0, 20)
+	
+	var tween2 = get_tree().create_tween()
+	tween2.tween_property(point_light2, "energy", 0, 20)
+	
+	
+	
 func evening_state():
 	var tween = get_tree().create_tween()
 	tween.tween_property(light, "energy", 0.95, 20)
+	
+	var tween1 = get_tree().create_tween()
+	tween1.tween_property(point_light, "energy", 1.5, 20)
+	
+	var tween2 = get_tree().create_tween()
+	tween2.tween_property(point_light2, "energy", 1.5, 20)
 
 func _on_day_night_timeout():
 	if state < 3:
